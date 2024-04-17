1a85d8e4366e26e03a85268fc6d270ba8a3ae841
krzyzinskim <mateusz_krzyzinski@wp.pl>
2024-04-17 17:46:06 +0200

    prepare experiment 1

--- experiments/experiment1.R
+++ experiments/experiment1.R
@@ -1,6 +1,7 @@
 library(survival)
 library(ranger)
 library(survex)
+devtools::load_all(".")
 
 set.seed(42)
 df <- read.csv("experiments/data/exp1_data_complex.csv")
@@ -24,83 +25,78 @@ explainer <- explain(model,
                      data = df[1:5],
                      y = Surv(df$time, df$event))
 
-performance <- model_performance(explainer)
-plot(performance)
-plot(performance, metrics_type = "scalar")
-
-x <- df[23, 1:5]
-x
-
 preds <- predict(explainer, explainer$data)
-original_pred <- predict(explainer, x)
-
-plot_predictions(rbind(preds, original_pred), explainer$times)
-
-plot_survival_weights(explainer, explainer$times, p=0, q=0)
 weights <- survival_weights(explainer, explainer$times, p=0, q=0)
+
 dists <- survival_distance_matrix(preds, explainer$times, weights)
 dendrogram <- get_clustering_dendrogram(dists)
 plot(dendrogram)
 
 plot(get_clustering_utilities(dendrogram, max_k = 6))
 
+plot_envelopes(preds, get_clusters(dendrogram, k=5),
+               alpha = 0.5,
+               explainer$times,
+               q = 0.05)
 
-plot_envelopes(preds, get_clusters(dendrogram, k=5), alpha = 0.5,
-               explainer$times, q = 0.05, original_pred)
-
-
-target_envelope <- get_envelope(preds, get_clusters(dendrogram, k=5),
+target_envelope_sf <- get_envelope(preds, get_clusters(dendrogram, k=5),
                                 cluster_id=5, q = 0.05)
+target_envelope_chf <- translate_target_envelope(target_envelope_sf)
+mu_target <- mean_time_to_survival(
+  explainer,
+  predictions = matrix(target_envelope_sf$lower_bound, nrow = 1))
 
 
-moc_res <- multiobjective_counterfactuals(explainer, x,
-                                          times = explainer$times,
-                                          target_envelope = target_envelope,
-                                          seed=23,
-                                          population_size=30,
-                                          max_generations=100,
-                                          weights = weights,
-                                          validity_threshold = 0,
-                                          verbose=TRUE,
-                                          tol_iter=4)
+not_in_target_cluster_ids <- which(get_clusters(dendrogram, k=5) != 5)
+tmp_df <- df[not_in_target_cluster_ids,]
 
-analyze(moc_res)
-plot_counterfactual_predictions(moc_res)
+k <- 50
+set.seed(42)
+sample_for_evaluation <- sample(1:nrow(tmp_df), k)
 
-new_target_envelope <- translate_target_envelope(target_envelope)
+moc_results <- list()
+tb_results <- list()
+kov_results <- list()
 
+moc_time <- numeric(k)
+tb_time <- numeric(k)
+kov_time <- numeric(k)
 
-tb_res <- treebased_counterfactuals(explainer, x,
-                                    times = explainer$times,
-                                    target_envelope = new_target_envelope,
-                                    k_paths = 20,
-                                    verbose = TRUE)
+for (i in seq_len(k)){
+  print(i)
+  obs <- tmp_df[sample_for_evaluation[i], 1:5]
 
-plot_counterfactual_predictions(tb_res)
+  moc_start <- Sys.time()
+  moc_results[[i]] <- multiobjective_counterfactuals(explainer, obs,
+                                                     times = explainer$times,
+                                                     target_envelope = target_envelope_sf,
+                                                     seed = i,
+                                                     population_size = 40,
+                                                     max_generations = 100,
+                                                     weights = weights,
+                                                     validity_threshold = 0,
+                                                     verbose = FALSE,
+                                                     tol_iter = 4)
+  moc_time[i] <- as.numeric(Sys.time() - moc_start, units = "secs")
 
+  tb_start <- Sys.time()
+  tb_results[[i]] <- treebased_counterfactuals(explainer, obs,
+                                               times = explainer$times,
+                                               target_envelope = target_envelope_chf,
+                                               max_counterfactuals = 40,
+                                               k_paths = 20,
+                                               verbose = FALSE)
+  tb_time[i] <- as.numeric(Sys.time() - tb_start, units = "secs")
 
-
-plot_envelopes(preds, get_clusters(dendrogram, k=5), alpha = 0.5,
-               explainer$times, q = 0.05, target_envelope$lower_bound)
-
-mu_target <- mean_time_to_survival(explainer, predictions = matrix(target_envelope$lower_bound, nrow = 1))
-mu_original <- mean_time_to_survival(explainer, predictions=original_pred)
-
-kov_res <- kovalev_method(explainer, x, seed=23, r = mu_original - mu_target, num_iter = 50)
-
-kov_res
-x
-
-plot_envelopes(preds, get_clusters(dendrogram, k=5), alpha = 0.5,
-               explainer$times, q = 0.05, predict(explainer, kov_res$z))
-
-mean_time_to_survival(explainer, predictions=predict(explainer, kov_res$z))
-
-
-results <- list()
-
-sample_for_evaluation <- sample(1:nrow(df), 0.2*nrow(df))
-
+  mu_original <- mean_time_to_survival(explainer, predictions = predict(explainer, obs))
+  kov_start <- Sys.time()
+  kov_results[[i]] <- kovalev_method(explainer, obs, seed=i,
+                                     r = mu_target - mu_original,
+                                     num_iter = 100,
+                                     num_particles = 200,
+                                     verbose = FALSE)
+  kov_time[i] <- as.numeric(Sys.time() - kov_start, units = "secs")
+}
 
 
 
