42f35a311a92b8a186bdf080f6a550f8cc089c69
krzyzinskim <mateusz_krzyzinski@wp.pl>
2024-04-17 17:45:51 +0200

    prepare better result

--- R/kovalev_method.R
+++ R/kovalev_method.R
@@ -16,7 +16,7 @@
 #' @import survex
 #' @export
 kovalev_method <- function(explainer, new_observation, r, num_iter=200, num_particles=1000,
-                           verbose=10, C=1e6, w=0.729, c1=1.4945, c2=1.4945, seed=NULL){
+                           verbose=10, C=1e6, w=0.729, c1=1.4945, c2=1.4945, tol_iter=10, seed=NULL){
   # theta
   theta <- ifelse(r >= 0, 1, -1)
   r <- abs(r)
@@ -56,6 +56,7 @@ kovalev_method <- function(explainer, new_observation, r, num_iter=200, num_part
   }
 
   # iteration 0 - initialization
+  invalid_counter <- 0
   set.seed(seed)
   particles <- data.frame(apply(data_range, 1, function(var_range) runif(num_particles, var_range[1], var_range[2])))
   particles <- restriction_procedure(x, particles, euclidean_distance_loss(x, particles), radius_closest, data_range)
@@ -70,6 +71,8 @@ kovalev_method <- function(explainer, new_observation, r, num_iter=200, num_part
                                      r, C, theta)
   best_loss <- min(best_losses)
   best_position <- best_positions[which.min(best_losses),]
+
+  history <- data.frame(generation = 0, loss = best_losses)
 
   for (i in 1:num_iter){
     # update velocities
@@ -89,10 +92,21 @@ kovalev_method <- function(explainer, new_observation, r, num_iter=200, num_part
     best_positions[losses < best_losses, ] <- particles[losses < best_losses, ]
     best_losses[losses < best_losses] <- losses[losses < best_losses]
 
+    history <- rbind(history, data.frame(generation = i, loss = best_losses))
+
     # update best loss
     if (min(best_losses) < best_loss){
       best_loss <- min(best_losses)
       best_position <- best_positions[which.min(best_losses), ]
+      invalid_counter <- 0
+    } else {
+      invalid_counter <- invalid_counter + 1
+      if (invalid_counter >= tol_iter){
+        warning(paste("Seach stopped due to no progress in the last",
+                      tol_iter,
+                      "iterations."))
+        break
+      }
     }
 
     if (is.numeric(verbose) & i %% verbose == 0){
@@ -104,13 +118,30 @@ kovalev_method <- function(explainer, new_observation, r, num_iter=200, num_part
     }
   }
 
-  list(
-    z = rescale_to_original(best_position, mu, sigma),
-    loss = best_loss,
-    mean_time_to_survival = mean_time_to_survival(explainer, rescale_to_original(best_position, mu, sigma)),
-    counterfactual_examples = rescale_to_original(best_positions, mu, sigma),
-    objective_value = best_losses
+  final_preds <- predict(explainer, rescale_to_original(best_positions, mu, sigma))
+  final_x_distances <- euclidean_distance_loss(x, best_positions)
+
+  objective_values <- data.frame(
+    loss = best_losses,
+    similarity = final_x_distances,
+    validity = (best_losses - final_x_distances) / C
   )
+
+  result <- list(
+    original_observation = new_observation,
+    r = r * theta,
+    times = explainer$times,
+    history = history,
+    original_prediction = predict(explainer, new_observation),
+    predictions = final_preds,
+    original_mean_time_to_survival = mean_value,
+    mean_times_to_survival = mean_time_to_survival(explainer, predictions = final_preds),
+    objective_values = objective_values,
+    counterfactual_examples = rescale_to_original(best_positions, mu, sigma),
+    best_counterfactual_example = rescale_to_original(best_position, mu, sigma)
+  )
+  class(result) <- c("counterfactual_explanations", "Kovalev_method_counterfactuals")
+  return(result)
 }
 
 
